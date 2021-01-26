d180c4c39f3acd62377e2cbaca9e00eb114d8e72
Italo Carlo <italocarlo@nti.ufal.br>
2021-01-26 15:57:28 -0300

    Meta tags Facebook

--- src/components/Metatags/index.js
+++ src/components/Metatags/index.js
@@ -5,7 +5,11 @@ export default function MetaTags({ image }) {
   return (
     <div>
       <ReactMetaTags>
-         <meta property="og:image" content={image} />
+         <meta property="og:type" content="website" />
+         <meta property="og:url" content="https://aluraquiz.italocarlo06.vercel.app/" />
+         <meta property="og:title" content="The Mandalorian" />
+         <meta property="og:description" content="" />
+         <meta property="og:image" content={image} />        
       </ReactMetaTags>
     </div>
   );
